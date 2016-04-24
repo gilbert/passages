e01050782090246061f8343d942487ab398e9865
Gilbert <gilbertbgarza@gmail.com>
2016-04-23 21:59:51 -0500

    Add linkable sections

--- site/typography.js
+++ site/typography.js
@@ -1,10 +1,24 @@
 (function(){
 
-  var wrapper = document.createElement('span')
-  wrapper.setAttribute('class', 'dialog')
+  var spanWrap = document.createElement('span')
+  spanWrap.setAttribute('class', 'dialog')
 
   findAndReplaceDOMText(document.querySelector('.box'), {
     find: /«[^»]+»/g,
-    wrap: wrapper
+    wrap: spanWrap
+  })
+
+  findAndReplaceDOMText(document.querySelector('.box'), {
+    find: /\(([0-9]+)\)/g,
+    replace: function (portion, match) {
+      var idWrap = document.createElement('a')
+
+      idWrap.setAttribute('class', 'section')
+      idWrap.setAttribute('id', 's-' + match[1])
+      idWrap.setAttribute('href', '#s-' + match[1])
+      idWrap.innerText = match[0]
+
+      return idWrap
+    }
   })
 })()
